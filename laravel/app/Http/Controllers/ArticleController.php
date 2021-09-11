fd939887fe74cde1c5f62315dc043ed320ba4659
yoshi3939 <irieyoshimi43@gmail.com>
2021-09-11 16:13:52 +0900

    記事投稿機能

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Article;
 
+use App\Http\Requests\ArticleRequest;
+
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -11,7 +13,19 @@ class ArticleController extends Controller
     public function index()
     {
         $articles = Article::all()->sortByDesc('created_at');
-        
         return view('articles.index', ['articles' => $articles]);
+    }
+
+    public function create()
+    {
+        return view('articles.create');    
+    }
+
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all()); 
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
     }
 }
