18943b28eac33b97009197ce424c27e84eb2b809
yoshi3939 <irieyoshimi43@gmail.com>
2021-09-15 18:31:19 +0900

    記事評価機能の実装追加

--- laravel/resources/js/components/ArticleLike.vue
+++ laravel/resources/js/components/ArticleLike.vue
@@ -5,11 +5,64 @@
       class="btn m-0 p-1 shadow-none"
     >
       <i class="fas fa-heart mr-1"
+      :class="{'red-text':this.isLikedBy}"
+      @click="clickLike" 
       />
     </button>
-    10
+     {{ countLikes }}
   </div>
 </template>
 
 <script>
+  export default {
+    props: {
+      initialIsLikedBy: {
+        type: Boolean,
+        default: false,
+      },
+      initialCountLikes: {
+        type: Number,
+        default: 0,
+      },
+
+      authorized: {
+        type: Boolean,
+        default: false,
+      },
+      endpoint: {
+        type: String,
+      },
+    },
+    data() {
+      return {
+        isLikedBy: this.initialIsLikedBy,
+        countLikes: this.initialCountLikes,
+      }
+    },
+
+    methods: {
+      clickLike() {
+        if (!this.authorized) {
+          alert('いいね機能はログイン中のみ使用できます')
+          return
+        }
+
+        this.isLikedBy
+          ? this.unlike()
+          : this.like()
+      },
+      async like() {
+        const response = await axios.put(this.endpoint)
+
+        this.isLikedBy = true
+        this.countLikes = response.data.countLikes
+      },
+      async unlike() {
+        const response = await axios.delete(this.endpoint)
+
+        this.isLikedBy = false
+        this.countLikes = response.data.countLikes
+      },
+    },
+  }
 </script>
