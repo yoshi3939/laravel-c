91582c43a75a7fdd414281457acb264745068832
yoshi3939 <irieyoshimi43@gmail.com>
2021-09-15 18:34:14 +0900

    記事評価機能アニメーション追加

--- laravel/resources/js/components/ArticleLike.vue
+++ laravel/resources/js/components/ArticleLike.vue
@@ -5,7 +5,7 @@
       class="btn m-0 p-1 shadow-none"
     >
       <i class="fas fa-heart mr-1"
-      :class="{'red-text':this.isLikedBy}"
+      :class="{'red-text':this.isLikedBy, 'animated heartBeat fast':this.gotToLike}"
       @click="clickLike" 
       />
     </button>
@@ -37,6 +37,7 @@
       return {
         isLikedBy: this.initialIsLikedBy,
         countLikes: this.initialCountLikes,
+        gotToLike: false,
       }
     },
 
@@ -56,12 +57,14 @@
 
         this.isLikedBy = true
         this.countLikes = response.data.countLikes
+        this.gotToLike = true
       },
       async unlike() {
         const response = await axios.delete(this.endpoint)
 
         this.isLikedBy = false
         this.countLikes = response.data.countLikes
+        this.gotToLike = false
       },
     },
   }
