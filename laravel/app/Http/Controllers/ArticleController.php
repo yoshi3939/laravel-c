28166977178f91cd63ae295f6559c73c27c2ad89
yoshi3939 <irieyoshimi43@gmail.com>
2021-09-18 16:30:06 +0900

    記事タグ投稿機能

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Article;
-
+use App\Tag;
 use App\Http\Requests\ArticleRequest;
-
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -31,6 +30,12 @@ class ArticleController extends Controller
         $article->fill($request->all()); 
         $article->user_id = $request->user()->id;
         $article->save();
+
+        $request->tags->each(function ($tagName) use ($article) {
+            $tag = Tag::firstOrCreate(['name' => $tagName]);
+            $article->tags()->attach($tag);
+        });
+        
         return redirect()->route('articles.index');
     }
 
