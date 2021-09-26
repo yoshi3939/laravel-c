4bb7d42e2fcb733dfe3ba98293d58fd6e9595ea4
yoshi3939 <irieyoshimi43@gmail.com>
2021-09-26 22:00:31 +0900

    ユーザーページに記事一覧を表示

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -7,6 +7,7 @@ use App\Notifications\PasswordResetNotification;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -44,6 +45,11 @@ class User extends Authenticatable
     public function sendPasswordResetNotification($token)
     {
         $this->notify(new PasswordResetNotification($token, new BareMail()));
+    }
+
+    public function articles(): HasMany
+    {
+        return $this->hasMany('App\Article');
     }
 
     public function followers(): BelongsToMany
